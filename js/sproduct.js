77227646eeee5d658e0c05a9ceaf345c5c7b19a2
Nitesh <niteshsinghbhandari8@gmail.com>
2023-07-24 02:45:34 +0530

    Repaired quantity bug

--- js/sproduct.js
+++ js/sproduct.js
@@ -1,29 +1,30 @@
-var MainImg = document.getElementById("MainImg");
-var smallimg = document.getElementsByClassName("small-img");
 
-smallimg[0].onclick = function(){
-    MainImg.src = smallimg[0].src;
-}
-smallimg[1].onclick = function(){
-    MainImg.src = smallimg[1].src;
-}
-smallimg[2].onclick = function(){
-    MainImg.src = smallimg[2].src;
-}
-smallimg[3].onclick = function(){
-    MainImg.src = smallimg[3].src;
-}
+    var MainImg = document.getElementById("MainImg");
+    var smallimg = document.getElementsByClassName("small-img");
 
-    // Function to update the price based on the quantity
+    smallimg[0].onclick = function () {
+        MainImg.src = smallimg[0].src;
+    };
+    smallimg[1].onclick = function () {
+        MainImg.src = smallimg[1].src;
+    };
+    smallimg[2].onclick = function () {
+        MainImg.src = smallimg[2].src;
+    };
+    smallimg[3].onclick = function () {
+        MainImg.src = smallimg[3].src;
+    };
+
     function updatePrice() {
         var price = 499; // Replace 499 with the initial price of the product
         var quantityInput = document.getElementById("quantity-input");
-        var quantity = parseInt(quantityInput.value);
+        var quantity = quantityInput.value.trim(); // Get the input value and remove leading/trailing spaces
 
-        // Check if the quantity is less than 1 and set it to 1
-        if (isNaN(quantity) || quantity < 1) {
-            quantity = 1;
-            quantityInput.value = quantity;
+        // Check if the quantity is empty or invalid
+        if (quantity === "" || isNaN(quantity) || quantity < 0) {
+            quantity = 0; // Set quantity to 0 if it's empty, less than 0, or not a number
+        } else {
+            quantity = parseInt(quantity); // Convert valid input to an integer
         }
 
         var totalPrice = price * quantity;
@@ -32,7 +33,10 @@ smallimg[3].onclick = function(){
         document.getElementById("product-price").innerText = "₹" + totalPrice;
     }
 
-    // Attach an event listener to the quantity input
-    document.getElementById("quantity-input").addEventListener("input", updatePrice);
+    // Attach event listener to handle input changes in the quantity input
+    document.getElementById("quantity-input").addEventListener("input", function (event) {
+        updatePrice();
+    });
 
-
+    // Call the updatePrice function initially to set the price based on the default quantity
+    updatePrice();
